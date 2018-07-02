226c548841f8d240a117b98c2e0f9794851252b3
Sune <fengel@gmail.com>
2018-07-02 21:49:04 +0200

    removed prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,20 +13,6 @@ module.exports = class extends Generator {
         )} generator!`
       )
     );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
   }
 
   writing() {
